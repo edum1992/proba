d12fc2d2512fa3f9643a6a237115dddbfdfba3ad
izarraga003 <zarragaemerando@gmail.com>
2016-09-28 09:55:38 +0200

    Eduren proba karpetan aldaketak egin ditut

--- hauteskundeak/src/com/isad/bertsioak/Hauteskundeak.java
+++ hauteskundeak/src/com/isad/bertsioak/Hauteskundeak.java
@@ -24,7 +24,7 @@ public class Hauteskundeak {
 		h2012.gehituMahaia("Irun");
 		h2012.gehituMahaia("Donostia");
 		h2012.gehituMahaia("Tolosa");
-		h2012.gehituMahaia("Zornotza");
+		h2012.gehituMahaia("Amorebieta");
 		
 		System.out.println(h2012);
 	}
